67a99b95a802b1088ebde733b75e764588a09c16
showkatali-dev <showkatali.dev@gmail.com>
2024-02-15 04:27:45 +0600

    updated

--- components/Portfolio/Projects.tsx
+++ components/Portfolio/Projects.tsx
@@ -5,7 +5,7 @@ const Projects = ({ projects }: { projects: Project[] }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-16">
       {projects.map((project) => (
-        <ProjectCard key={project.name} project={project} />
+        <ProjectCard key={project._id} project={project} />
       ))}
     </div>
   );
